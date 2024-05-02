11913bf7ec065a3c191d23939327b0c7315942df
lunatic280 <https://github.com/lunatic280>
2024-05-02 09:31:27 +0900

    2024-05-01

--- 1920.py
+++ 1920.py
@@ -1,13 +1,27 @@
-n = int(input())
-arr_n = list(map(int, input().split()))
-m = int(input())
-arr_m = list(map(int, input().split()))
-result = []
+import sys
+n = int(sys.stdin.readline())
+arr_n = list(map(int, sys.stdin.readline().split()))
+arr_n.sort()
+m = int(sys.stdin.readline())
+arr_m = list(map(int, sys.stdin.readline().split()))
 
-for i in range(m):
-    if arr_m[i] in arr_n:
-        result.append(1)
+def test(n, arr_n, index):
+    
+    left = 0
+    right = n - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr_n[mid] == index:
+            return True
+        elif arr_n[mid] > index:
+            right = mid - 1
+        elif arr_n[mid] < index:
+            left = mid + 1
+    return False
+    
+
+for i in arr_m:
+    if test(n, arr_n, i):
+        print(1)
     else:
-        result.append(0)
-
-print(result)
+        print(0)
